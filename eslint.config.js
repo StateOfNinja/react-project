39372fa47d4e14d77e6ca0aa3eb577aaf7a93f43
StateOfNinja <catbape@gmail.com>
2025-02-25 01:04:57 +0300

    внес изменения

---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import importPlugin from 'eslint-plugin-import';
+import babelParser from '@babel/eslint-parser';
+
+export default [
+  js.configs.recommended,
+  {
+    ignores: ['node_modules', 'dist', 'build'],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react'],
+        },
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        document: 'readonly',
+        window: 'readonly',
+      },
+    },
+
+    plugins: {
+      react,
+      import: importPlugin,
+    },
+    rules: {
+      indent: ['off', 2],
+      'linebreak-style': [0, 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': ['warn', { varsIgnorePattern: 'React' }],
+      'react/prop-types': 'off',
+      'import/no-unresolved': ['error', { caseSensitive: false }],
+      'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          moduleDirectory: ['node_modules', 'src/'],
+        },
+      },
+    },
+  },
+];
